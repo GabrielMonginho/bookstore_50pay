523c6dec2fcbcf67a821320c7d2d7cb8ddb5f772
Gabriel Leal Monginho <z0050pay>
2024-12-03 11:13:14 +0000

    Initial Commit @12/3/2024, 11:13:14 AM

---
+++ bookstore_50pay/appfiori_50pay/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("appfiori50pay.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
